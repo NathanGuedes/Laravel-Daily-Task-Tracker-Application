bb796478cbbae389513d3cc586b8bc779cd5b802
Nathn Guedes Pessoa <nathan.g.p@outlook.com>
2026-03-18 15:36:59 -0300

    Create and fix Model Task, Category with your migrations

---
+++ app/Models/Task.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property int|null $category_id
+ * @property string $title
+ * @property string|null $description
+ * @property bool $is_recurring
+ * @property Carbon|null $task_date
+ * @property string|null $is_completed
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Category|null $category
+ * @property-read User $user
+ * @method static Builder<static>|Task newModelQuery()
+ * @method static Builder<static>|Task newQuery()
+ * @method static Builder<static>|Task query()
+ * @method static Builder<static>|Task whereCategoryId($value)
+ * @method static Builder<static>|Task whereCreatedAt($value)
+ * @method static Builder<static>|Task whereDescription($value)
+ * @method static Builder<static>|Task whereId($value)
+ * @method static Builder<static>|Task whereIsCompleted($value)
+ * @method static Builder<static>|Task whereIsRecurring($value)
+ * @method static Builder<static>|Task whereTaskDate($value)
+ * @method static Builder<static>|Task whereTitle($value)
+ * @method static Builder<static>|Task whereUpdatedAt($value)
+ * @method static Builder<static>|Task whereUserId($value)
+ * @mixin Eloquent
+ */
+class Task extends Model
+{
+    protected function casts(): array
+    {
+        return [
+            'is_recurring' => 'boolean',
+            'completed_at' => 'datetime',
+            'task_date' => 'datetime',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): belongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
